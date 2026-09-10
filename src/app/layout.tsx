38e85064ad9ec672c8d402bfbfd65309aa499db1
shafaf Sadic <shafaf410@gmail.com>
2026-09-10 21:32:49 +0530

    Complete brand typography hierarchy: Syne (Hero Display), Space Grotesk (Section Titles), Plus Jakarta Sans (Body), JetBrains Mono (Tech Accents)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300..800;1,300..800&family=Space+Grotesk:wght@500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,600&family=Space+Grotesk:wght@400;500;600;700&family=Syne:wght@700;800&display=swap"
           rel="stylesheet"
         />
       </head>
